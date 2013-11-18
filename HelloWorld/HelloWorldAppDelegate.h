833adbea2171ca9a95d9253103144985c7a41f87
Abdul <abdul@Abduls-MacBook-Air.local>
2013-11-18 12:09:56 +0200

    Initial Commit

---
+++ HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by Abdul on 11/18/13.
+//  Copyright (c) 2013 Abdul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Abdul on 11/18/13.
+//  Copyright (c) 2013 Abdul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
